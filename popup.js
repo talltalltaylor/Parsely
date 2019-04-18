f5a8287e9d2791173b8dac447286c1c9dfd42411
Taylor Robbins <40040427+talltalltaylor@users.noreply.github.com>
2019-04-17 19:43:11 -0500

    updated style, added pics for btns, fixed events

--- popup.js
+++ popup.js
@@ -1,43 +1,55 @@
-var parseButton = document.getElementById('button_parse');
-var settingsButton = document.getElementById('button_settings');
-var helpButton = document.getElementById('button_help');
-var settingsButton = document.getElementById('button_settings');
-var colorButton = document.createElement("BUTTON");
-var fontButton = document.createElement("BUTTON");
-var plainButton = document.createElement("BUTTON");
-var line = document.createElement("HR");
 
-document.addEventListener("DOMContentLoaded", function(event) {
+
+document.addEventListener('DOMContentLoaded', function() {
+	
+	var parseButton = document.getElementById('button_parse');
+	parseButton.classList.add('button');
+	var settingsButton = document.getElementById('settingsIcon');
+	settingsButton.classList.add("button")
+	var helpButton = document.getElementById('button_help');
+	helpButton.classList.add("button")
+	var colorButton = document.createElement("BUTTON");
+	var fontButton = document.createElement("BUTTON");
+	var plainButton = document.createElement("BUTTON");
+	var line = document.createElement("HR");
+
 	settingsButton.addEventListener('click', onSettingsClicked);
 	helpButton.addEventListener('click', onHelpClicked);
-});
-
-function onHelpClicked (){
-  console.log('help button worked');
-  var help_url = "/readme.html";
-  chrome.tabs.create({url: help_url});
-}
-
-function onSettingsClicked(){
-	console.log('settings button worked');
-	colorButton.innerText = "Change Color";
-	fontButton.innerText = "Change Font";
-	plainButton.innerText = "Plain Text Mode";
-	document.body.appendChild(line);
-	document.body.appendChild(colorButton);
-	document.body.appendChild(fontButton);
-	document.body.appendChild(plainButton);
-	settingsButton.removeEventListener('click', onSettingsClicked);
-	settingsButton.addEventListener('click', removeSettings)
 	
-}
+	function onHelpClicked (){
+		console.log('help button worked');
+		var help_url = "/readme.html";
+		chrome.tabs.create({url: help_url});
+	}
 
-function removeSettings(){
-	document.body.removeChild(colorButton)
-	document.body.removeChild(fontButton)
-	document.body.removeChild(plainButton)
-	settingsButton.removeEventListener('click', removeSettings);
-	settingsButton.addEventListener('click', onSettingsClicked);
-}
+	function onSettingsClicked(){
+		console.log('settings button worked');
+		colorButton.innerText = "Change Color";
+		colorButton.classList.add("button")
+		fontButton.innerText = "Change Font";
+		fontButton.classList.add("button")
+		plainButton.innerText = "Plain Text Mode";
+		plainButton.classList.add("button")
+		document.body.appendChild(line);
+		document.body.appendChild(colorButton);
+		document.body.appendChild(fontButton);
+		document.body.appendChild(plainButton);
+		settingsButton.removeEventListener('click', onSettingsClicked);
+		settingsButton.addEventListener('click', removeSettings)
+	}
+
+	function removeSettings(){
+		document.body.removeChild(colorButton)
+		document.body.removeChild(fontButton)
+		document.body.removeChild(plainButton)
+		document.body.removeChild(line);
+		settingsButton.removeEventListener('click', removeSettings);
+		settingsButton.addEventListener('click', onSettingsClicked);
+	}
+  
+ });
+
+
+
 //need to add any popup functionality here
 //can take JS out of html and put it in here
