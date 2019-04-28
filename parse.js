ef4daa370e972987b459630db92bcf66e9bc9960
Taylor Robbins <40040427+talltalltaylor@users.noreply.github.com>
2019-04-28 16:50:36 -0500

    added 5 more websites that work

--- parse.js
+++ parse.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
     if(window.location.href.indexOf("thestayathomechef") > -1) {
-        
         $(".entry-content").children().not(".wprm-recipe-container").toggle();
         $("style").hide();
         $(".adthrive-player-container adthrive-collapse-player").toggle();
@@ -9,9 +8,42 @@ $( document ).ready(function() {
         $(".entry-comments").toggle();
         $(".comment-respond").toggle();
     }
+    else if(window.location.href.indexOf("spendwithpennies") > -1) {
+        
+        $(".entry-content").children().not(".wprm-recipe-container").toggle();
+        $("style").hide();
+        $(".adthrive-player-container adthrive-collapse-player").toggle();
+    }
     else if(window.location.href.indexOf("simplyrecipes") > -1) {
         $(".entry-content").children().not(".recipe-callout").toggle();
         $("script").hide();
         $(".comment-section").toggle();
     }
+    else if(window.location.href.indexOf("cookingclassy") > -1) {
+        
+        $(".content").children().not(".wprm-recipe-container").toggle();
+        $("style").hide();
+        $(".adthrive-player-container adthrive-collapse-player").toggle();
+        $(".adthrive-ad adthrive-dynamic").toggle();
+    }
+    else if(window.location.href.indexOf("belleofthekitchen") > -1) {
+        
+        $(".entry-content").children().not(".wprm-recipe-container").toggle();
+        $("style").hide();
+        $(".adthrive-player-container adthrive-collapse-player").toggle();
+    }
+    else if(window.location.href.indexOf("averiecooks") > -1) {
+        
+        $(".wrapper").children().children().children().not(".recipe").children().not(".mv-create-wrapper").toggle();
+        $("script").hide();
+        $("p").toggle();
+        $("h2").toggle();
+    }
+    else if(window.location.href.indexOf("damndelicious") > -1) {
+        
+        $(".wrapper").children().children().children().not(".recipe").toggle();
+        $(".adthrive-player-container adthrive-collapse-player").toggle();
+        $("script").hide();
+    }
+    
 });
