bcd0b6056fbb86921e3f948b25cb709d200f164e
Taylor Robbins <40040427+talltalltaylor@users.noreply.github.com>
2019-04-27 20:15:59 -0500

    works with simplyrecipes.com

--- parse.js
+++ parse.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
- 
-    $(".entry-content").children().not(".wprm-recipe-container").toggle();
-    $(".entry-comments").toggle();
+    if(window.location.href.indexOf("thestayathomechef") > -1) {
+        
+        $(".entry-content").children().not(".wprm-recipe-container").toggle();
+        $("style").hide();
+        $(".adthrive-player-container adthrive-collapse-player").toggle();
+        $(".tlod").children().toggle();
+        $(".widget-wrap").toggle();
+        $(".entry-comments").toggle();
+        $(".comment-respond").toggle();
+    }
+    else if(window.location.href.indexOf("simplyrecipes") > -1) {
+        $(".entry-content").children().not(".recipe-callout").toggle();
+        $("script").hide();
+        $(".comment-section").toggle();
+    }
 });
