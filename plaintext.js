25b6078c7ff88fe29482cfa84641ed88bd7d2cc1
rlg118 <47229469+rlg118@users.noreply.github.com>
2019-05-01 18:49:21 -0500

    Updated the plain text mode
    Changed it from PHP to text mode.

---
+++ plaintext.js
@@ -0,0 +1,79 @@
+// AUTHOR: Rebecca Geng and Philip Fitzgerald 
+//
+//
+$( document ).ready(function() {
+    if(window.location.href.indexOf("thestayathomechef") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+    }
+    else if(window.location.href.indexOf("spendwithpennies") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+  
+    }
+    else if(window.location.href.indexOf("simplyrecipes") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+    }
+    else if(window.location.href.indexOf("cookingclassy") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+    }
+    else if(window.location.href.indexOf("belleofthekitchen") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+  
+    }
+    else if(window.location.href.indexOf("averiecooks") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+    }
+    else if(window.location.href.indexOf("damndelicious") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+    }
+    else if(window.location.href.indexOf("gimmesomeoven") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+     
+    }
+    else if(window.location.href.indexOf("recipetineats") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text()
+
+    }
+    else if(window.location.href.indexOf("tastesbetterfromscratch") > -1) {
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text() 
+    }
+});
