1d2ae63ca57607512756bf5a92da0bfb47bbf73a
rlg118 <47229469+rlg118@users.noreply.github.com>
2019-05-01 21:32:37 -0500

    Update plaintext.js

--- plaintext.js
+++ plaintext.js
@@ -3,11 +3,11 @@
 //
 $( document ).ready(function() {
     if(window.location.href.indexOf("thestayathomechef") > -1) {
-      .clone()
-      .children()
-      .remove()
-      .end()
-      .text()
+      .clone() // clone the element 
+      .children() // select all the children 
+      .remove() // remove all the children 
+      .end() // go back to selected element 
+      .text() // get the text of element 
     }
     else if(window.location.href.indexOf("spendwithpennies") > -1) {
       .clone()
