7600c345fb7f6b1cc11a13b0a41007c14b020768
Taylor Robbins <40040427+talltalltaylor@users.noreply.github.com>
2019-05-01 22:09:08 -0500

    deleted extra spaces, added missing semicolons,

--- popup.js
+++ popup.js
@@ -1,79 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	
-
 	var parseButton = document.getElementById('button_parse');
-
 	parseButton.classList.add('button');
-
 	var settingsButton = document.getElementById('settingsIcon');
-
-	settingsButton.classList.add("button")
-
+	settingsButton.classList.add("button");
 	var helpButton = document.getElementById('button_help');
-
-	helpButton.classList.add("button")
-
+	helpButton.classList.add("button");
 	var colorButton = document.createElement("BUTTON");
-
 	var fontButton = document.createElement("BUTTON");
-
 	var plainButton = document.createElement("BUTTON");
-
 	var line = document.createElement("HR");
 
-
-
-	parseButton.addEventListener('click', parse)
-
+	parseButton.addEventListener('click', parse);
 	settingsButton.addEventListener('click', onSettingsClicked);
-
 	helpButton.addEventListener('click', onHelpClicked);
-	
 	plainButton.addEventListener('click', plainText); 
-
 	
 
 	function onHelpClicked (){
 
 		console.log('help button worked');
-
 		var help_url = "/readme.html";
-
 		chrome.tabs.create({url: help_url});
-
 	}
-
 
 
 	function onSettingsClicked(){
 
 		console.log('settings button worked');
-
 		colorButton.innerText = "Change Color";
-
-		colorButton.classList.add("button")
-
+		colorButton.classList.add("button");
 		fontButton.innerText = "Change Font";
-
-		fontButton.classList.add("button")
-
+		fontButton.classList.add("button");
 		plainButton.innerText = "Plain Text Mode";
-
 		plainButton.classList.add("button")
-
 		document.body.appendChild(line);
-
 		document.body.appendChild(colorButton);
-
 		document.body.appendChild(fontButton);
-
 		document.body.appendChild(plainButton);
-
 		settingsButton.removeEventListener('click', onSettingsClicked);
-
 		settingsButton.addEventListener('click', removeSettings)
-
 	}
 
 
@@ -81,17 +47,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	function removeSettings(){
 
 		document.body.removeChild(colorButton)
-
 		document.body.removeChild(fontButton)
-
 		document.body.removeChild(plainButton)
-
 		document.body.removeChild(line);
-
 		settingsButton.removeEventListener('click', removeSettings);
-
 		settingsButton.addEventListener('click', onSettingsClicked);
-
 	}
 
 
@@ -99,19 +59,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	function toggleBack(){
 
 		chrome.tabs.executeScript({file: 'jquery-3.4.0.slim.js'});
-
 		chrome.tabs.executeScript({file: 'parse.js'});
-
 		document.getElementById('parsely').style.color = '#73a753';
-
 		document.body.style.backgroundColor = 'white';
-
 		parseButton.removeEventListener('click', toggleBack);
-
 		parseButton.addEventListener('click', parse);
-
-
-
 	}
 
 
@@ -119,18 +71,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	function parse(){
 
 		chrome.tabs.executeScript({file: 'jquery-3.4.0.slim.js'});
-
 		chrome.tabs.executeScript({file: 'parse.js'});
-
 		document.getElementById('parsely').style.color = 'black';
-
 		document.body.style.backgroundColor = '#73a753';
-
 		parseButton.removeEventListener('click', parse);
-
 		parseButton.addEventListener('click', toggleBack);
-
 	}
+	
 	function plainText(){
 		chrome.tabs.executeScript({file: 'jquery-3.4.0.slim.js'});
 		chrome.tabs.executeScript({file: 'plaintext.js'});
@@ -140,13 +87,5 @@ document.addEventListener('DOMContentLoaded', function() {
 		plainButton.addEventListener('click', toggleBack);
 	}
   
-
  });
   
-
-
-
-
-
-//need to add any popup functionality here
-//can take JS out of html and put it in here
