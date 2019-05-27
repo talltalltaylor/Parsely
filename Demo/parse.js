1d4e36cc1bd69a8c8b47e1cc226a95aee7c88202
Taylor <tw0mp@calcifer.grandenetworks.net>
2019-05-26 21:04:00 -0500

    made parse more generic, added sites

--- Demo/parse.js
+++ Demo/parse.js
@@ -1,72 +1,96 @@
 //Author: Taylor Robbins
 //
-// basic multi-way branch based on website
-// that toggles or hides all elements that aren't the specified recipe class
-// need a more generic way of doing this, but this will work for now
-//
+// 4 different parse functions, each one tailored to a specific DOM structure
+// multi-way branch based on website, such that parse cannot be initiated on the wrong sites
+// recipe is stored before looking at which website and passed to specific parse
+// used jquery selectors instead of literal class names
+
 $( document ).ready(function() {
-    if(window.location.href.indexOf("thestayathomechef") > -1) {
-        $(".entry-content").children().not(".wprm-recipe-container").toggle();
+    // get recipe and domain
+    var recipe = $("div").find("*[class*='recipe']");
+    var domain = window.location.hostname;
+
+    // lists of websites that will work with Parsely
+    var list1 = ["thestayathomechef.com", "www.spendwithpennies.com", "www.cookingclassy.com", 
+    "belleofthekitchen.com", "www.recipetineats.com", "www.mysuburbankitchen.com", "divascancook.com"];
+    var list2 = ["www.averiecooks.com", "damndelicious.net"];
+    var list3 = ["www.simplyrecipes.com", "tastesbetterfromscratch.com", "www.loveandlemons.com", 
+    "minimalistbaker.com", "www.skinnytaste.com", "cafedelites.com", "shewearsmanyhats.com", 
+    "cookieandkate.com", "sugarspunrun.com", "spicysouthernkitchen.com"];
+    var list4 = ["www.gimmesomeoven.com", "www.wellplated.com"];
+
+    // check lists for domain, then parse
+    if (list1.includes(domain)){
+        parse1(recipe);
+    }
+    else if(list2.includes(domain)){
+        parse2(recipe);
+        if(domain.indexOf("averie") > -1){
+            pAndH2();
+        }
+    }
+    else if(list3.includes(domain)){
+        parse3(recipe);
+    }
+    else if(list4.includes(domain)){
+        parse4(recipe);
+    }
+
+    // parse1 looks at children of divs that end with 'content'
+    function parse1(recipe){
+        $("div[class$='content']").children().not(recipe).toggle();
         $("style").hide();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-        $(".tlod").children().toggle();
+        $("script").hide();
+        $("div[class*='adthrive' i]").toggle();
+        $("img[class*='wp-image' i]").toggle();
+        $("div[class*='hidden']").toggle();
+        $(".tlod").toggle();
         $(".widget-wrap").toggle();
-        $(".entry-comments").toggle();
-        $(".comment-respond").toggle();
+        $("div[class*='comment']").toggle();   
     }
-    else if(window.location.href.indexOf("spendwithpennies") > -1) {
-        
-        $(".entry-content").children().not(".wprm-recipe-container").toggle();
+    // parse2 looks at children of articles that end with 'content'
+    function parse2(recipe){
+        $("article[class$='content']").children().not(recipe).toggle();
         $("style").hide();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-    }
-    else if(window.location.href.indexOf("simplyrecipes") > -1) {
-        $(".entry-content").children().not(".recipe-callout").toggle();
         $("script").hide();
-        $(".comment-section").toggle();
+        $("div[class*='adthrive' i]").toggle();
+        $("div[class*='hidden']").toggle();
+        $(".tlod").toggle();
+        $(".widget-wrap").toggle();
+        $("div[class*='comment']").toggle();   
     }
-    else if(window.location.href.indexOf("cookingclassy") > -1) {
-        
-        $(".content").children().not(".wprm-recipe-container").toggle();
+    // parse3 looks at children of divs that end with 'entry-content'
+    function parse3(recipe){
+        $("div[class$='entry-content']").children().not(recipe).toggle();
+        $("div[class$='description']").toggle();
         $("style").hide();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-        $(".adthrive-ad adthrive-dynamic").toggle();
-    }
-    else if(window.location.href.indexOf("belleofthekitchen") > -1) {
-        
-        $(".entry-content").children().not(".wprm-recipe-container").toggle();
-        $("style").hide();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-    }
-    else if(window.location.href.indexOf("averiecooks") > -1) {
-        
-        $(".wrapper").children().children().children().not(".recipe").children().not(".mv-create-wrapper").toggle();
         $("script").hide();
+        $("div[class*='adthrive' i]").toggle();
+        $("img[class*='wp-image' i]").toggle();
+        $("div[class*='hidden']").toggle();
+        $(".tlod").toggle();
+        $(".widget-wrap").toggle();
+        $("div[class*='comment']").toggle();   
+    }
+    // parse4 looks at children of divs that contain 'singlepost'
+    function parse4(recipe){
+        $("div[class*='singlepost']").children().not(recipe).toggle();
+        $("div[class$='description']").toggle();
+        $("style").hide();
+        $("script").hide();
+        $("div[class*='adthrive' i]").toggle();
+        $("img[class*='wp-image' i]").toggle();
+        $("div[class*='hidden']").toggle();
+        $(".tlod").toggle();
+        $(".widget-wrap").toggle();
+        $("div[class*='comment']").toggle();   
+    }
+    // function to hide p and h2 elements
+    // was using this often, but ended up using it less and less
+    // as the code improved. Now it's only for one site
+    function pAndH2(){
         $("p").toggle();
         $("h2").toggle();
     }
-    else if(window.location.href.indexOf("damndelicious") > -1) {
-        
-        $(".wrapper").children().children().children().not(".recipe").toggle();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-        $("script").hide();
-    }
-    else if(window.location.href.indexOf("gimmesomeoven") > -1) {
-        
-        $(".wrapper").children().children().children().not(".recipe").toggle();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-        $("script").hide();
-    }
-    else if(window.location.href.indexOf("recipetineats") > -1) {
-        
-        $(".entry-content").children().not(".wprm-recipe-container").toggle();
-        $("style").hide();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-    }
-    else if(window.location.href.indexOf("tastesbetterfromscratch") > -1) {
-        
-        $(".entry-content").children().not(".wprm-recipe-container").toggle();
-        $("style").hide();
-        $(".adthrive-player-container adthrive-collapse-player").toggle();
-    }
+
 });
--- Demo/parse_gen.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $("div[*='content']").children().not(".[*='recipe']").toggle();
-})
